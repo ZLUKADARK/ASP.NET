267041a9809082063d11d5bb797e0e823ee26687
ZLUKADARK <ZLUKA@DESKTOP-Q20QITO>
2021-04-08 15:59:03 +0600

    LAB_5

---
+++ WebApplication1.DAL/Entities/Dish.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.DAL.Entities
+{
+    public class Dish
+    {
+        public int DishId { get; set; } // id блюда
+        public string DishName { get; set; } // название блюда
+        public string Description { get; set; } // описание блюда
+        public int Calories { get; set; } // кол. калорий на порцию
+        public string Image { get; set; } // имя файла изображения
+                                          // Навигационные свойства
+        /// <summary>
+        /// группа блюд (например, супы, напитки и т.д.)
+        /// </summary>
+        public int DishGroupId { get; set; }
+        public DishGroup Group { get; set; }
+    }
+    public class DishGroup
+    {
+        public int DishGroupId { get; set; }
+        public string GroupName { get; set; }
+        /// <summary>
+        /// Навигационное свойство 1-ко-многим
+        /// </summary>
+        public List<Dish> Dishes { get; set; }
+    }
+}
---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL.Entities;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Dish> _dishes;
+        List<DishGroup> _dishGroups;
+        public ProductController()
+        {
+            SetupData();
+        }
+        public IActionResult Index()
+        {
+            return View(_dishes);
+        }
+        /// <summary>
+        /// Инициализация списков
+        /// </summary>
+        private void SetupData()
+        {
+            _dishGroups = new List<DishGroup>
+                {
+                    new DishGroup {DishGroupId=1, GroupName="Стартеры"},
+                    new DishGroup {DishGroupId=2, GroupName="Салаты"},
+                    new DishGroup {DishGroupId=3, GroupName="Супы"},
+                    new DishGroup {DishGroupId=4, GroupName="Основные блюда"},
+                    new DishGroup {DishGroupId=5, GroupName="Напитки"},
+                    new DishGroup {DishGroupId=6, GroupName="Десерты"}
+                };
+            _dishes = new List<Dish>
+                {
+                    new Dish {DishId = 1, DishName="Грибной суп",
+                    Description="Лук, картофель, сливки, грибы шампиньоны.",
+                    Calories =180, DishGroupId=3, Image="Soup1.png" },
+                    new Dish { DishId = 2, DishName="Томатный суп",
+                    Description="Лук, чеснок, помидоры, итальянские травы, перец",
+                    Calories =330, DishGroupId=3, Image="Soup2.jpg" },
+                    new Dish { DishId = 3, DishName="Салат сыттов",
+                    Description="Фасоль, корейская морковь, сыр, сухари, зелень.",
+                    Calories =120, DishGroupId=4, Image="Salad.png" },
+                    new Dish { DishId = 4, DishName="Салат с тунцом",
+                    Description="Тунец, салат, помидор. Подается в пите из пшеничной муки",
+                    Calories =240, DishGroupId=4, Image="Salad2.jpg" },
+                    new Dish { DishId = 5, DishName="Морс Gedonia клюква",
+                    Description="500 мл, Вода очищенная, клюква, сахар, брусника.",
+                    Calories =55, DishGroupId=5, Image="Mors.png" },
+                    new Dish { DishId = 5, DishName="Fanta",
+                    Description="500 мл, Газированный напиток со вкусом апельсина.",
+                    Calories =40, DishGroupId=5, Image="Fanta.png" }
+                };
+        }
+    }
+}
